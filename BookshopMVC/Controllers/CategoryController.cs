518551b0ae2029627e84fbcd90bd190922bcd747
adrianrakiec <rakiecadrian17@gmail.com>
2024-07-02 17:07:08 +0200

    feat: add Delete method to CategoryController and delete view

--- BookshopMVC/Controllers/CategoryController.cs
+++ BookshopMVC/Controllers/CategoryController.cs
@@ -38,7 +38,7 @@ namespace BookshopMVC.Controllers
 
             return View();
         }
-        
+
         public IActionResult Edit(int? id)
         {
             if (id == null || id == 0)
@@ -68,6 +68,39 @@ namespace BookshopMVC.Controllers
             }
 
             return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? category = _context.Categories.FirstOrDefault(u => u.Id == id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? category = _context.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
