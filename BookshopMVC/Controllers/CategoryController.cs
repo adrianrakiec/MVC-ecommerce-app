86660b266b017d9204c9ca7437009ffd94ad6cbb
adrianrakiec <rakiecadrian17@gmail.com>
2024-07-02 14:59:58 +0200

    build: create Category controller and basic view

---
+++ BookshopMVC/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using BookshopMVC.Data;
+using BookshopMVC.Models.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookshopMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categories = _context.Categories.ToList();
+
+            return View(categories);
+        }
+    }
+}
