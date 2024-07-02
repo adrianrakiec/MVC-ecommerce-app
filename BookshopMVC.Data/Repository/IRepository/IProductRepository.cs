17c0ca31520dfe83765033641c8d6f1cdefd93e9
adrianrakiec <rakiecadrian17@gmail.com>
2024-07-02 23:10:12 +0200

    build: create ProductRepository and add it to UnitOfWork

---
+++ BookshopMVC.Data/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using BookshopMVC.Models.Models;
+
+namespace BookshopMVC.Data.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Update(Product obj);
+    }
+}
