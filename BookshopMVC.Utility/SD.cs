4fe4d16b3e332b6534ffca65915cfb51ef5f34c2
adrianrakiec <rakiecadrian17@gmail.com>
2024-07-04 23:06:03 +0200

    feat: add order and payment statuses

--- BookshopMVC.Utility/SD.cs
+++ BookshopMVC.Utility/SD.cs
@@ -8,7 +8,22 @@ namespace BookshopMVC.Utility
 {
     public class SD
     {
+        //Roles
         public const string Role_Customer = "Customer";
         public const string Role_Admin = "Admin";
-    }
+
+		//Order status
+		public const string StatusPending = "Pending";
+		public const string StatusApproved = "Approved";
+		public const string StatusInProcess = "Processing";
+		public const string StatusShipped = "Shipped";
+		public const string StatusCancelled = "Cancelled";
+		public const string StatusRefunded = "Refunded";
+
+		//Payment status
+		public const string PaymentStatusPending = "Pending";
+		public const string PaymentStatusApproved = "Approved";
+		public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+		public const string PaymentStatusRejected = "Rejected";
+	}
 }
