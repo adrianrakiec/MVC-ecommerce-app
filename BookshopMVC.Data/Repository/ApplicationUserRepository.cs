84e7a88f94d6b379889045a1630ed5170eb86532
adrianrakiec <rakiecadrian17@gmail.com>
2024-07-04 22:36:50 +0200

    build: create ApplicationUser repository and add to UnitOfWork

---
+++ BookshopMVC.Data/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using BookshopMVC.Data.Data;
+using BookshopMVC.Data.Repository.IRepository;
+using BookshopMVC.Models.Models;
+
+namespace BookshopMVC.Data.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDbContext _context;
+        public ApplicationUserRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
--- BookshopMVC.Data/Repository/UnitOfWork.cs
+++ BookshopMVC.Data/Repository/UnitOfWork.cs
@@ -14,11 +14,13 @@ namespace BookshopMVC.Data.Repository
         public ICategoryRepository Category { get; private set; }
         public IProductRepository Product { get; private set; }
         public IShoppingCartRepository ShoppingCart { get; private set; }
+        public IApplicationUserRepository ApplicationUser { get; private set; }
         public IOrderHeaderRepository OrderHeader { get; private set; }
         public IOrderDetailRepository OrderDetail { get; private set; }
         public UnitOfWork(ApplicationDbContext context)
         {
             _context = context;
+            ApplicationUser = new ApplicationUserRepository(_db);
             ShoppingCart = new ShoppingCartRepository(_context);
             Category = new CategoryRepository(_context);
             Product = new ProductRepository(_context);
