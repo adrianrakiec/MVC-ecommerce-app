c4d5e8bbbcd8e6d2669d908514fe8521bf1714d8
adrianrakiec <rakiecadrian17@gmail.com>
2024-07-04 20:20:08 +0200

    build: create CartController, Cart view and ShoppingCart view model

---
+++ BookshopMVC/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,46 @@
+﻿using BookshopMVC.Data.Repository.IRepository;
+using BookshopMVC.Models.Models;
+using BookshopMVC.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BookshopMVC.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+                includeProperties: "Product")
+            };
+
+            foreach(var cart in ShoppingCartVM.ShoppingCartList)
+            {
+                cart.Price = GetPrice(cart);
+                ShoppingCartVM.OrderTotal += cart.Price * cart.Count;
+            }
+
+            return View(ShoppingCartVM);
+        }
+
+        private double GetPrice(ShoppingCart shoppingCart)
+        {
+            return shoppingCart.Product.Price;
+        }
+    }
+}
