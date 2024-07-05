0cebc5a8a42b4934b85512a5aded9bc7c9992215
adrianrakiec <rakiecadrian17@gmail.com>
2024-07-05 11:16:37 +0200

    feat: add order filter

--- BookshopMVC/Areas/Admin/Controllers/OrderController.cs
+++ BookshopMVC/Areas/Admin/Controllers/OrderController.cs
@@ -1,5 +1,6 @@
 ﻿using BookshopMVC.Data.Repository.IRepository;
 using BookshopMVC.Models.Models;
+using BookshopMVC.Utility;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookshopMVC.Areas.Admin.Controllers
@@ -14,22 +15,29 @@ namespace BookshopMVC.Areas.Admin.Controllers
 			_unitOfWork = unitOfWork;
 		}
 
-		public IActionResult Index()
+		public IActionResult Index(string status)
 		{
-            List<OrderHeader> orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            IEnumerable<OrderHeader> orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeaders = orderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
 
             return View(orderHeaders);
 		}
-
-
-		#region API CALLS
-
-		[HttpGet]
-		public IActionResult GetAll()
-		{
-			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
-			return Json(new { data = objOrderHeaders });
-		}
-		#endregion
 	}
 }
